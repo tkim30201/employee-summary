3b845fe5a56b9e12bd38141da51bc3937cb11e35
tkim30201 <63823948+tkim30201@users.noreply.github.com>
2020-09-28 08:54:26 -0400

    updated app.js

--- app.js
+++ app.js
@@ -10,26 +10,122 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const employees = []
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+let typeQuestions = [{
+    type: 'list',
+    name: 'memberRole',
+    message: 'Confirm the role of the team member:',
+    choices: ['Engineer', 'Intern', 'Manager']
+},
+{
+    type: 'input',
+    name: 'name',
+    message: 'Enter the name of the employee:'
+},
+{
+    type: 'input',
+    name: 'memId',
+    message: 'Enter the employee ID number:',
+    default: 'Employee'
+},
+{
+    type: 'input',
+    name: 'email',
+    message: 'Enter the Team Member\'s email address:'
+}
+];
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+let internQuestions = [{
+    type: 'input',
+    name: 'school',
+    message: 'Enter the name of your school/university'
+}];
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+let engineerQuestions = [{
+    type: 'input',
+    name: 'github',
+    message: 'Enter GitHub username:'
+}];
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+let managerQuestions = [{
+    type: 'input',
+    name: 'officNumber',
+    message: 'Enter manager\'s offic number'
+}];
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+function createEngineer(name, id, email) {
+    return inquirer.prompt(engineerQuestions)
+        .then(({
+            github
+        }) => {
+            let newEmployee = new Engineer(name, id, email, github)
+            employees.push(newEmployee);
+        });
+}
+
+function createIntern(name, id, email) {
+    return inquirer.prompt(internQuestions)
+        .then(({
+            school
+        }) => {
+            let newEmployee = new Intern(name, id, email, school)
+            employees.push(newEmployee)
+        });
+}
+
+function createManager(name, id, email) {
+    return inquirer.prompt(managerQuestions)
+        .then(({
+            officeNumber
+        }) => {
+            let newEmployee = new Manager(name, id, email, officeNumber);  
+            employees.push(newEmployee);
+        });
+}
+
+function createTeam() {
+    const htmlData = render(employees)
+    fs.writeFileSync(outputPath, htmlData, function (err) {
+        if (err) {
+            return console.log(err)
+        }
+    })
+}
+
+async function main() {
+    inquirer.prompt(typeQuestions).then(async({
+        name,
+        employeeId,
+        email,
+        employeeOption
+    }) => {
+        switch (employeeOption) {
+            case 'Engineer':
+                await createEngineer(name, employeeId, email);
+                break;
+            case 'Intern':
+                await createIntern(name, employeeId, email);
+                break;
+            case 'Manager':
+                await createManager(name, employeeId, email);
+                break;
+        }
+        inquirer.prompt([{
+            type: 'list',
+            name: 'isComplete',
+            choices: ['Yes', 'No'],
+            message: "Is your team complete?"
+        }]).then(({
+            isComplete
+        }) => {
+            if (isComplete === "Yes") {
+                createTeam(employees)
+            } else {
+                main()
+            }
+        })
+    })
+}
+
+main()
